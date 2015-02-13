ea6a18a7dc9b16671544d40cfdcc686be51ea947
Jurgen te Stroet <jurgen.testroet@gmail.com>
2015-02-13 23:56:49 +0100

    read xml-file to xmlDocument with JUnitTest

--- src/research/xquery/ResearchXQuery.java
+++ src/research/xquery/ResearchXQuery.java
@@ -5,10 +5,36 @@
  */
 package research.xquery;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
 /**
  *
  * @author Jurgen
  */
 public class ResearchXQuery {
+    
+    private Document xmlDocument;
+    
+    public ResearchXQuery(){ 
+        DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = null;
+        try {
+            builder = builderFactory.newDocumentBuilder();
+            xmlDocument = builder.parse(
+                        new FileInputStream
+                                ("/Users/Jurgen/GitRepos/ResearchJurgen/resources/top10nlv2_WMS.xml")
+                            );
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            e.printStackTrace();  
+        }     
+   }
+    
+    
     
 }
---
+++ test/research/xquery/ResearchXQueryTest.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package research.xquery;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Jurgen
+ */
+public class ResearchXQueryTest {
+    
+    public ResearchXQueryTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        ResearchXQuery researchXQuery = new ResearchXQuery();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
