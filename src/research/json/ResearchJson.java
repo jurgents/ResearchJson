6b58a5c6ea357caaf01eb09c693d7aaeb5ea17b0
Jurgen te Stroet <jurgen.testroet@gmail.com>
2015-02-13 21:26:48 +0100

    add package research query

--- src/research/json/ResearchJson.java
+++ src/research/json/ResearchJson.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package researchjson;
+package research.json;
 
 import java.util.HashMap;
 import java.util.Iterator;
